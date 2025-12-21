81be0a9a4169f2f1ca87ccd4f9b8480c4249697d
Nick DiVona <skatedivona@gmail.com>
2025-12-20 17:19:08 -0800

    update icon and naming

--- src/main/java/com/nylofreeze/NyloFreezePlugin.java
+++ src/main/java/com/nylofreeze/NyloFreezePlugin.java
@@ -5,23 +5,22 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
-import net.runelite.api.SpriteID;
 import net.runelite.api.events.GameTick;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.game.SpriteManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
+import net.runelite.client.util.ImageUtil;
 import java.awt.image.BufferedImage;
 
 @Slf4j
 @PluginDescriptor(name = "Nylo Freeze Indicator", description = "Shows freeze chance for Nylocas Matomenos in ToB", tags = {
         "tob", "theatre", "maiden", "freeze", "nylo" })
 public class NyloFreezePlugin extends Plugin {
-    // Relevant Theatre of Blood region IDs
+    // Theatre of Blood region IDs
     private static final int MAIDEN_REGION = 12613;
-    private static final int VER_SINHAZA_REGION = 14642;
+    private static final int VER_SINHAZA_REGION = 14642; // Ver Sinhaza (ToB bank area)
 
     @Inject
     private Client client;
@@ -34,9 +33,6 @@ public class NyloFreezePlugin extends Plugin {
 
     @Inject
     private FreezeCalculator freezeCalculator;
-
-    @Inject
-    private SpriteManager spriteManager;
 
     private NyloFreezeInfoBox infoBox;
 
@@ -60,11 +56,19 @@ public class NyloFreezePlugin extends Plugin {
 
         int regionId = client.getLocalPlayer().getWorldLocation().getRegionID();
 
-        // Only show in the Maiden's room/hall or Ver Sinhaza
-        if (isAtMaidenOrToBBank(regionId)) {
+        // Only show in Maiden room or Ver Sinhaza bank
+        if (isInToBOrBank(regionId)) {
             if (infoBox == null) {
-                // Load Ice Barrage spell icon
-                spriteManager.getSpriteAsync(SpriteID.SPELL_ICE_BARRAGE, 0, this::createInfoBox);
+                // Load the nylo icon from resources
+                BufferedImage icon = ImageUtil.loadImageResource(getClass(), "/nylo_icon.png");
+
+                if (icon != null) {
+                    infoBox = new NyloFreezeInfoBox(icon, config, freezeCalculator, this);
+                    infoBoxManager.addInfoBox(infoBox);
+                    log.info("InfoBox created with nylo icon");
+                } else {
+                    log.warn("Failed to load nylo icon from resources");
+                }
             } else {
                 // Update existing infobox
                 infoBox.update();
@@ -74,17 +78,6 @@ public class NyloFreezePlugin extends Plugin {
         }
     }
 
-    private void createInfoBox(BufferedImage icon) {
-        if (icon == null) {
-            log.warn("Failed to load Ice Barrage icon");
-            return;
-        }
-
-        infoBox = new NyloFreezeInfoBox(icon, config, freezeCalculator, this);
-        infoBoxManager.addInfoBox(infoBox);
-        log.info("InfoBox created with Ice Barrage icon");
-    }
-
     private void removeInfoBox() {
         if (infoBox != null) {
             infoBoxManager.removeInfoBox(infoBox);
@@ -92,9 +85,8 @@ public class NyloFreezePlugin extends Plugin {
         }
     }
 
-    private boolean isAtMaidenOrToBBank(int regionId) {
-        return regionId == MAIDEN_REGION
-                || regionId == VER_SINHAZA_REGION;
+    private boolean isInToBOrBank(int regionId) {
+        return regionId == MAIDEN_REGION || regionId == VER_SINHAZA_REGION;
     }
 
     @Provides
